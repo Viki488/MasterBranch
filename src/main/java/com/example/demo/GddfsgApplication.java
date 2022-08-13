8a174a562b248bc967c975e667bf0c8076f85a1b
Viki488 <cahudhari.viki66@gmail.com>
2022-08-13 12:01:31 +0530

    Upadated

---
+++ src/main/java/com/example/demo/GddfsgApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GddfsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GddfsgApplication.class, args);
+	}
+
+}
